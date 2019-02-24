2803b86ad1cdc4362992340112ad68285a151590
Dan Drinkard <dan.drinkard@gmail.com>
2019-02-24 13:09:58 -0500

    more debugging

--- googmusic/intents/selection.py
+++ googmusic/intents/selection.py
@@ -92,6 +92,8 @@ def play_search_radio(query):
         return statement('Sorry, no results for %s' % query)
 
     tracks = client.get_station_tracks(station['seed'][matcher], num_tracks=500)
+    print(station)
+    print(matcher)
     print(tracks)
     music_queue.clear()
     for track in tracks:
